e1cde75d7bee538e2c78583d4d3b3d3165331920
Surkus <v.surkus@me.com>
2022-02-08 18:45:25 +0200

    Initial commit

---
+++ cs24_paskaita_Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cs24_paskaita_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cs24_PASKAITA_Generics!");
+            #region TEORIJA - GENERICS
+
+            //
+            //
+            //
+
+            #endregion
+        }
+    }
+}
+
